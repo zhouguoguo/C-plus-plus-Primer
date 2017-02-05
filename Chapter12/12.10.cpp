e6c2c5b6e4b446641582a9668f18ac0b6f620c3a
Zhou Liang <cherry861002@gmail.com>
2017-02-05 22:43:16 +0800

    update 12.10

--- Chapter12/12.10.cpp
+++ Chapter12/12.10.cpp
@@ -6,16 +6,27 @@ void process(shared_ptr<int> sp)
 {
 	cout << "inside: ";
 	cout << *sp << endl;
+	cout << sp.use_count() << endl;
 }
 
 int main()
 {
 	shared_ptr<int> p(new int(42));
+	cout << p.use_count() << endl;
+//	cout << *p << endl;
 //	process(p);
 //	process(shared_ptr<int>(p));
 	int *q = p.get();
-	process(shared_ptr<int>(q)); // cause Aborted (core dumped)
-	cout << *q << endl;
+//	cout << q << endl;
+
+	shared_ptr<int> temp(q); //temp计数1， q计数1，即使它们指向同一个对象
+	cout << p.use_count() << endl;
+
+	process(temp); // temp释放了对象，q计数仍然为1， 但是对象已被释放
+//	cout << *p << endl;
+	cout << p.use_count() << endl;
+//	cout << q << endl;
+	
 
 	int* a = new int(5);
 	process(shared_ptr<int>(a));  
@@ -27,7 +38,7 @@ int main()
 	-2144456920 --> memory is deleted
 	inside: 5
 	-2144456920 --> memory is deleted
-	Aborted (core dumped)  --> why?
+	Aborted (core dumped)  --> 
 	*/
 
 	return 0;
